f2b36b8efce01eb64d16ec329da6595e7d33badc
Shawnmichaeldelima <113750053+Shawnmichaeldelima@users.noreply.github.com>
2022-09-24 09:16:58 +0800

    Add files via upload

---
+++ const[1].dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// Text colors
+const cTextHeaderColor = Color(0xff2A2A2A);
+const cTextFeatureColor = Color(0xff707070);
+const cTextLabelColor = Colors.white;
+
+// Detail for profile item
+const cProfileTopic = 'Personal Background';
+const cProfileFeature =
+    "Name:Shawn Michael T. Delima\n Age: 20\n Gender: Male\n Address: Malabago, Calasiao, Pangasinan\n Contact No: 09075202519\n Date of Birth: October 12, 2001\n" +
+        "Nationality: Filipino\n Civil Status: Single";
+const cProfileLabel = 'Profile';
+const cProfilePicture = 'assets/images/avatar.png';
+const cProfileIconImage = 'assets/images/profile.png';
+const cProfileColor = Color(0xFF0D40A1);
+const cProfileBegColor = Color(0xFF1e88e5);
+const cProfileEndColor = Color(0xFF64b5f6);
+
+// Detail for education item
+const cEducationTopic = 'Educational Background';
+const cEducationFeature =
+    'Bachelor of Information Technology\n Philippine College of Science and Technology-March 2019-2023\n' +
+        'Certificates:\n' +
+        '- NCII Computer System Servicing\n' +
+        '- Game Developer\n' +
+        '- Road to Success';
+const cEducationLabel = 'Education';
+const cEducationPicture = 'assets/images/book.png';
+const cEducationIconImage = 'assets/images/education.png';
+const cEducationColor = Color(0xFF00b8d4);
+const cEduBegColor = Color(0xFF18ffff);
+const cEduEndColor = Color(0xFF84ffff);
+
+// Detail for experience item
+const cExperienceTopic = ' Personal Skills ';
+const cExperienceFeature = '- Can work under pressure\n' +
+    '- Can incode\n' +
+    '- Creative and has critical-thinking skills\n' +
+    '- Can Assist';
+const cExperienceLabel = 'Skills';
+const cExperiencePicture = 'assets/images/laptop.png';
+const cExperienceIconImage = 'assets/images/experience.png';
+const cExperienceColor = Color(0xFF1b5e20);
+const cExpBegColor = Color(0xFF00e676);
+const cExpEndColor = Color(0xFF81c784);
+
+final cCaptionTextSize = 20.0.sp;
+const cStartingIndex = 0;
